71cf598f5d8ad7dd40c45cc270d14a1dff230032
glasspark <olleh125@naver.com>
2023-07-26 15:59:56 +0900

    첫 커밋

---
+++ src/main/java/com/project/leisure/hyokyeong/partner/PartnerController.java
@@ -0,0 +1,165 @@
+package com.project.leisure.hyokyeong.partner;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.project.leisure.hyokyeong.user.UserListService;
+import com.project.leisure.taeyoung.user.RegPartner;
+import com.project.leisure.taeyoung.user.RegRepository;
+import com.project.leisure.taeyoung.user.RegService;
+import com.project.leisure.taeyoung.user.UserRepository;
+import com.project.leisure.yuri.product.Accommodation;
+import com.project.leisure.yuri.product.AccommodationService;
+import com.project.leisure.yuri.product.Product;
+import com.project.leisure.yuri.product.ProductService;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@Controller
+@RequestMapping("/admin")
+public class PartnerController {
+
+	private final PartnerListService partnerListService;
+	private final RegService regService;
+	private final RegRepository regRepository;
+	private final HttpServletRequest request;
+	private final UserListService userListService;
+	private final UserRepository userRepository;
+	private final AccomdationListService accomdationListService;
+	private final ProductService productService;
+	private final AccommodationService accommodationService;
+
+	public PartnerController(PartnerListService partnerListService, RegService regService, RegRepository regRepository,
+			UserListService userListService, HttpServletRequest request, UserRepository userRepository,
+			AccomdationListService accomdationListService, ProductService productService,
+			AccommodationService accommodationService) {
+		this.partnerListService = partnerListService;
+		this.regService = regService;
+		this.regRepository = regRepository;
+		this.request = request;
+		this.userListService = userListService;
+		this.userRepository = userRepository;
+		this.accomdationListService = accomdationListService;
+		this.productService = productService;
+		this.accommodationService = accommodationService;
+	}
+
+	@GetMapping("/partnerListPage")
+	public String getPartnerList(Model model, @RequestParam(value = "page", defaultValue = "0") int page,
+			@RequestParam(value = "kw", defaultValue = "") String kw) {
+		Page<RegPartner> partnerPage = this.partnerListService.getPartnerRegPage(page, kw);
+		List<String> fileList = this.partnerListService.getFileList();
+		model.addAttribute("paging", partnerPage);
+		model.addAttribute("fileList", fileList);
+		model.addAttribute("kw", kw);
+		model.addAttribute("totalPages", partnerPage.getTotalPages());
+		partnerListService.partnerList(model);
+		model.addAttribute("partners", partnerPage.getContent());
+
+		return "khk/partnerListPage";
+	}
+
+	// 등록 업체 리스트
+	@GetMapping("/accomdationListPage")
+	public String getAccomdationList(Model model, @RequestParam(value = "page", defaultValue = "0") int page,
+			@RequestParam(value = "kw", defaultValue = "") String kw) {
+		Page<Accommodation> accomdationListPage = this.accomdationListService.getAccommodationList(page, kw);
+		model.addAttribute("paging", accomdationListPage);
+		model.addAttribute("kw", kw);
+		model.addAttribute("totalPages", accomdationListPage.getTotalPages());
+		accomdationListService.accommodationList(model);
+		model.addAttribute("accomdations", accomdationListPage.getContent());
+
+		return "khk/accomdationListPage";
+	}
+
+	@GetMapping("/showFile")
+	public ResponseEntity<Resource> showFile(@RequestParam("filename") String filename, HttpServletRequest request)
+			throws IOException {
+		String fileUrl = partnerListService.getFileUrlFromDatabase(filename);
+		if (fileUrl != null) {
+			String baseUrl = partnerListService.getBaseUrl(request);
+			String fullUrl = baseUrl + fileUrl;
+
+			Resource resource = new UrlResource(fullUrl);
+			if (resource.exists() && resource.isReadable()) {
+				HttpHeaders headers = new HttpHeaders();
+				headers.setContentType(MediaType.IMAGE_JPEG);
+				return new ResponseEntity<>(resource, headers, HttpStatus.OK);
+			}
+		}
+		throw new FileNotFoundException("파일을 찾을 수 없습니다.");
+	}
+
+	@PostMapping("/approve/{id}")
+	public String handleApproval(@PathVariable("id") Long id, @RequestParam("status") int status,
+			HttpServletRequest request) {
+		partnerListService.handleApproval(id, status);
+
+		// 이전 페이지 URL 가져오기
+		String referer = request.getHeader("Referer");
+
+		// 이전 페이지로 리다이렉트
+		return "redirect:" + referer;
+	}
+
+//	// 해당 숙소를 삭제
+//		@DeleteMapping(value = "deleteAcc/{id}")
+//		public ResponseEntity<?> deleteAcc(@PathVariable("id") Long acc_id) {
+//
+//			// 해당 숙소에 연결된 상품들 조회
+//			List<Product> products = productService.findProductsByAccommodationId(acc_id);
+//
+//			// 숙소 연결된 이미지 삭제 해당 숙소둘의 Pk를 가져온다
+//
+//			// 각 상품에 연결된 이미지들 삭제 및 상품 삭제
+//			for (Product product : products) {
+//				productService.pdDelete(product.getProduct_id());
+//			}
+//
+//			// 숙소 삭제
+//			accommodationService.deleteAcc(acc_id);
+//
+//			return ResponseEntity.ok().build(); // HTTP 200 상태 코드를 반환
+//		}
+
+	// 해당 숙소를 삭제
+	@DeleteMapping(value = "deleteAcc/{accId}")
+	public ResponseEntity<?> deleteAcc(@PathVariable("accId") Long acc_id) {
+		try {
+
+			// 해당 숙소에 연결된 상품들 조회
+			List<Product> products = productService.findProductsByAccommodationId(acc_id);
+
+			// 각 상품에 연결된 이미지들 삭제 및 상품 삭제
+			for (Product product : products) {
+				productService.pdDelete(product.getProduct_id());
+			}
+
+			// 숙소 삭제
+			accommodationService.deleteAcc(acc_id);
+
+			return ResponseEntity.ok().build(); // HTTP 200 상태 코드를 반환
+		} catch (RuntimeException e) {
+			// 예외 발생 시, 적절한 오류 상태 코드를 반환
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Accommodation not found");
+		}
+	}
+}
---
+++ src/main/resources/static/js/kty_filter.js
@@ -0,0 +1,323 @@
+
+
+document.addEventListener("DOMContentLoaded", function() {
+	const pageElements = document.getElementsByClassName("page-link");
+	Array.from(pageElements).forEach(function(element) {
+		element.addEventListener('click', function() {
+			document.getElementById('page').value = this.dataset.page;
+			document.getElementById('searchForm').submit();
+		});
+	});
+
+
+	document.addEventListener("DOMContentLoaded", function() {
+		const pageElements = document.getElementsByClassName("page-link2");
+		Array.from(pageElements).forEach(function(element) {
+			element.addEventListener('click', function() {
+				document.getElementById('page').value = this.dataset.page;
+				document.getElementById('kw2').value = document.getElementById('region_dropdown').value;
+				const selectedFoodTypes = Array.from(document.querySelectorAll('input[name="foodType"]:checked')).map(el => el.value);
+				document.getElementById('kw3').value = selectedFoodTypes.join(',');
+				document.getElementById('searchForm2').submit();
+			});
+		});
+		document.addEventListener("DOMContentLoaded", function() {
+			const pageElements = document.getElementsByClassName("page-link3");
+			Array.from(pageElements).forEach(function(element) {
+				element.addEventListener('click', function() {
+					document.getElementById('page').value = this.dataset.page;
+					document.getElementById('kw2').value = document.getElementById('region_dropdown').value;
+
+					document.getElementById('searchForm3').submit();
+
+				});
+
+			});
+		});
+
+
+		document.addEventListener("DOMContentLoaded", function() {
+			const pageElements = document.getElementsByClassName("page-link4");
+			Array.from(pageElements).forEach(function(element) {
+				element.addEventListener('click', function() {
+					document.getElementById('page').value = this.dataset.page;
+					document.getElementById('kw2').value = document.getElementById('region_dropdown').value;
+					const selectedFoodTypes = Array.from(document.querySelectorAll('input[name="ParkingType"]:checked')).map(el => el.value);
+					document.getElementById('kw4').value = selectedFoodTypes.join(',');
+					document.getElementById('searchForm4').submit();
+
+				});
+
+			});
+		});
+
+
+		document.addEventListener("DOMContentLoaded", function() {
+			const pageElements = document.getElementsByClassName("page-link5");
+			Array.from(pageElements).forEach(function(element) {
+				element.addEventListener('click', function() {
+					document.getElementById('page').value = this.dataset.page;
+					document.getElementById('kw2').value = document.getElementById('region_dropdown').value;
+					const selectedFoodTypes = Array.from(document.querySelectorAll('input[name="foodType"]:checked')).map(el => el.value);
+					document.getElementById('kw3').value = selectedFoodTypes.join(',');
+					const selectedFoodTypes2 = Array.from(document.querySelectorAll('input[name="ParkingType"]:checked')).map(el => el.value);
+					document.getElementById('kw4').value = selectedFoodTypes.join(',');
+					document.getElementById('searchForm5').submit();
+
+				});
+
+			});
+		});
+
+		const regionDropdown = document.getElementById('region_dropdown');
+		const foodTypeCheckboxes = document.querySelectorAll('input[name="foodType"]');
+		regionDropdown.addEventListener('change', function() {
+			document.getElementById('kw2').value = this.value;
+			document.getElementById('searchForm2').submit();
+		});
+		foodTypeCheckboxes.forEach(function(checkbox) {
+			checkbox.addEventListener('change', function() {
+				const selectedFoodTypes = Array.from(document.querySelectorAll('input[name="foodType"]:checked')).map(el => el.value);
+				document.getElementById('kw3').value = selectedFoodTypes.join(',');
+				document.getElementById('searchForm2').submit();
+			});
+		});
+	});
+
+
+	const btnSearch = document.getElementById("btn_search_food");
+	btnSearch.addEventListener('click', function() {
+		document.getElementById('kw').value = document.getElementById('search_food').value;
+		document.getElementById('page').value = 0;
+		document.getElementById('searchForm').submit();
+	});
+
+
+
+	const btnSearch2 = document.getElementById("searchBtn");
+	btnSearch2.addEventListener('click', function() {
+		const regionDropdown = document.getElementById("region_dropdown");
+		const selectedRegion = regionDropdown.value;
+
+		const checkboxes = document.querySelectorAll('input[name="foodType"]:checked');
+		const selectedFoodTypes = Array.from(checkboxes).map(checkbox => checkbox.value);
+
+		const checkboxes2 = document.querySelectorAll('input[name="ParkingType"]:checked');
+		const selectedFoodTypes2 = Array.from(checkboxes2).map(checkbox => checkbox.value);
+
+		if (selectedRegion && selectedFoodTypes.length > 0) {
+			let url = "/tour/daegu_food2";
+			url += "?kw2=" + encodeURIComponent(selectedRegion);
+			url += "&kw3=" + encodeURIComponent(selectedFoodTypes.join(','));
+			window.location.href = url;
+		} else if (selectedRegion && selectedFoodTypes2.length > 0) {
+			let url = "/tour/daegu_food4";
+			url += "?kw2=" + encodeURIComponent(selectedRegion);
+			url += "&kw4=" + encodeURIComponent(selectedFoodTypes2.join(','));
+			window.location.href = url;
+		}
+
+		else if (selectedRegion) {
+			let url = "/tour/daegu_food3";
+			url += "?kw2=" + encodeURIComponent(selectedRegion);
+			window.location.href = url;
+		}
+	});
+
+	const btnSearch3 = document.getElementById("searchBtn");
+	btnSearch2.addEventListener('click', function() {
+		const regionDropdown = document.getElementById("region_dropdown");
+		const selectedRegion = regionDropdown.value;
+
+		const checkboxes = document.querySelectorAll('input[name="foodType"]:checked');
+		const selectedFoodTypes = Array.from(checkboxes).map(checkbox => checkbox.value);
+
+		const checkboxes2 = document.querySelectorAll('input[name="ParkingType"]:checked');
+		const selectedFoodTypes2 = Array.from(checkboxes2).map(checkbox => checkbox.value);
+		if (selectedRegion && selectedFoodTypes.length > 0 && selectedFoodTypes2.length > 0) {
+			let url = "/tour/daegu_food5";
+			url += "?kw2=" + encodeURIComponent(selectedRegion);
+			url += "&kw3=" + encodeURIComponent(selectedFoodTypes.join(','));
+			url += "&kw4=" + encodeURIComponent(selectedFoodTypes2.join(','));
+			window.location.href = url;
+		}
+	});
+
+
+
+
+
+
+
+	// Extract opening hours and compare with current time
+	const foodEntries = document.getElementsByClassName("food-entry");
+	Array.from(foodEntries).forEach(function(entry) {
+		const openingHours = entry.getElementsByClassName("shop-opening-text")[0].textContent.trim().split(" ~ ");
+		const currentTime = new Date();
+		const currentHour = currentTime.getHours();
+
+		const openingTime = parseInt(openingHours[0].split(":")[0]);
+		const closingTime = parseInt(openingHours[1].split(":")[0]);
+
+		const resultOpening = entry.getElementsByClassName("result_opening")[0];
+		if ((currentHour >= openingTime && currentHour < closingTime) ||
+			(closingTime < openingTime && (currentHour < closingTime || currentHour >= openingTime))) {
+			resultOpening.textContent = "영업 중";
+		} else {
+			resultOpening.textContent = "영업 종료";
+		}
+	});
+
+});
+
+
+document.addEventListener("DOMContentLoaded", function() {
+	const foodShopPhotos = document.getElementsByClassName("food_shop_photo");
+
+	Array.from(foodShopPhotos).forEach(function(element) {
+		// Get the total number of images in the folder
+		const numImages = 100; // Replace with the actual number of images in the folder
+
+		// Generate a random number between 1 and numImages
+		const randomNumber = Math.floor(Math.random() * numImages) + 1;
+
+		// Set the background image URL
+		const imageURL = `/img/tour/food/shop/${randomNumber}.jpg`;
+
+		// Set the background image for the current element
+		element.style.backgroundImage = `url('${imageURL}')`;
+	});
+
+
+
+
+/*  아래 수정 필요*/
+
+function getAddressFromLatLng(lat, lng, callback) {
+  var geocoder = new kakao.maps.services.Geocoder();
+
+  geocoder.coord2Address(lng, lat, function(result, status) {
+    if (status === kakao.maps.services.Status.OK) {
+      var address = result[0].address.address_name;
+      callback(address);
+    } else {
+      console.log("주소 변환 중 오류가 발생했습니다.");
+    }
+  });
+}
+
+
+function compareDistance(address1, address2, callback) {
+  // Implement your distance comparison logic here
+  // This function should calculate the distance between two addresses and invoke the callback with the result
+  // For simplicity, let's assume the distance is a random number
+  var distance = Math.random() * 10; // Replace this with your actual distance calculation
+
+  callback(distance);
+}
+
+
+function showPosition(position) {
+  var latitude = position.coords.latitude;
+  var longitude = position.coords.longitude;
+
+  getAddressFromLatLng(latitude, longitude, function(currentAddress) {
+    var foodEntries = document.getElementsByClassName("food-entry");
+
+    // Iterate over each food entry
+    for (var i = 0; i < foodEntries.length; i++) {
+      var foodEntry = foodEntries[i];
+      var foodAddressElement = foodEntry.querySelector(".shop-address");
+      var foodAddress = foodAddressElement.innerText;
+
+      // Compare the distance between the current address and the food address
+      compareDistance(currentAddress, foodAddress, function(distance) {
+        // Update the distance result in the corresponding element
+        var sddElement = foodEntry.querySelector("#sdd");
+        var formattedDistance = formatDistance(distance);
+        sddElement.innerText = formattedDistance;
+      });
+    }
+  });
+}
+
+
+
+function formatDistance(distance) {
+  if (distance >= 1) {
+    // Convert to kilometers and format with two decimal places
+    var formattedDistance = distance.toFixed(2) + " km";
+    return formattedDistance;
+  } else {
+    // Convert to meters and format without decimal places
+    var formattedDistance = (distance * 1000).toFixed(0) + " m";
+    return formattedDistance;
+  }
+}
+
+function showError(error) {
+  switch (error.code) {
+    case error.PERMISSION_DENIED:
+      console.log("사용자가 위치 공유 권한을 거부했습니다.");
+      break;
+    case error.POSITION_UNAVAILABLE:
+      console.log("위치 정보를 사용할 수 없습니다.");
+      break;
+    case error.TIMEOUT:
+      console.log("위치 정보 요청이 시간 초과되었습니다.");
+      break;
+    case error.UNKNOWN_ERROR:
+      console.log("알 수 없는 오류가 발생했습니다.");
+      break;
+  }
+}
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition, showError);
+  } else {
+    console.log("Geolocation이 지원되지 않는 브라우저입니다.");
+  }
+}
+
+window.onload = getLocation;
+		
+		
+		
+		function showPosition(position) {
+  var latitude = position.coords.latitude;
+  var longitude = position.coords.longitude;
+
+  getAddressFromLatLng(latitude, longitude, function(currentAddress) {
+    var currentMyDongnae = document.getElementById("current_my_dongnae");
+    currentMyDongnae.innerHTML = currentAddress;
+
+    var foodEntries = document.getElementsByClassName("food-entry");
+
+    // Iterate over each food entry
+    for (var i = 0; i < foodEntries.length; i++) {
+      var foodEntry = foodEntries[i];
+      var foodAddressElement = foodEntry.querySelector(".shop-address");
+      var foodAddress = foodAddressElement.innerText;
+
+      // Compare the distance between the current address and the food address
+      compareDistance(currentAddress, foodAddress, function(distance) {
+        // Update the distance result in the corresponding element
+        var sddElement = foodEntry.querySelector("#sdd");
+        var formattedDistance = formatDistance(distance);
+        sddElement.innerText = formattedDistance;
+      });
+    }
+  });
+}
+		
+	
+	
+	
+		
+});
+
+
+
+
+
