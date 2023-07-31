07188ab509bb0645f051827733c228ea929b685c
taeyoung0504 <tang050405@gmail.com>
2023-07-31 16:28:49 +0900

    취소요청 완료

--- src/main/java/com/project/leisure/taeyoung/user/CancelRequestService.java
+++ src/main/java/com/project/leisure/taeyoung/user/CancelRequestService.java
@@ -1,18 +1,26 @@
 package com.project.leisure.taeyoung.user;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CancelRequestService {
-    private final CancelRequestRepository cancelRequestRepository;
+	private final CancelRequestRepository cancelRequestRepository;
 
-    @Autowired
-    public CancelRequestService(CancelRequestRepository cancelRequestRepository) {
-        this.cancelRequestRepository = cancelRequestRepository;
-    }
+	@Autowired
+	public CancelRequestService(CancelRequestRepository cancelRequestRepository) {
+		this.cancelRequestRepository = cancelRequestRepository;
+	}
 
-    public CancelRequest saveCancelRequest(CancelRequest cancelRequest) {
-        return cancelRequestRepository.save(cancelRequest);
-    }
+	public CancelRequest saveCancelRequest(CancelRequest cancelRequest) {
+		return cancelRequestRepository.save(cancelRequest);
+	}
+
+	public List<CancelRequest> getCancleReq() {
+		Sort sort = Sort.by(Sort.Direction.DESC, "id"); // Assuming "id" is the field used for sorting
+		return cancelRequestRepository.findAll(sort);
+	}
 }
