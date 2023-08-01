8d5910bfb6c1d8fc1e4c4b98d5ef4aeb93593ec6
Kim Do Gyeom <sexypig91@gmail.com>
2023-08-01 10:44:46 +0900

    url 수정

--- src/main/java/com/project/leisure/dogyeom/booking/reserveList/JpaAlreadyReserveRepository.java
+++ src/main/java/com/project/leisure/dogyeom/booking/reserveList/JpaAlreadyReserveRepository.java
@@ -25,7 +25,7 @@ public interface JpaAlreadyReserveRepository extends JpaRepository<BookingVO, In
             "   OR (:nextDate BETWEEN b.checkin AND b.checkOut)) " +
             "    AND b.tempAccomId = :tempAccomId " +
             "    AND (:currentDate <> b.checkOut AND :nextDate <> b.checkin)" +
-			" AND b.bookStatus IS NOT NULL AND b.bookStatus != 'CANCEL_PAYMENT' AND b.bookStatus != '이용완료' AND b.bookStatus != 'CANCELED' OR b.bookStatus = '이용중'")
+			" AND b.bookStatus IS NOT NULL AND b.bookStatus != 'CANCEL_PAYMENT' AND b.bookStatus != '이용완료' AND b.bookStatus != 'CANCELED' AND b.bookStatus = '이용중'")
 	List<BookingVO> findTempRoomId(
             @Param("tempAccomId") Long tempAccomId,
             @Param("currentDate") LocalDate currentDate,
