27134102be843cd25bb329d12b27667348b4b623
taeyoung0504 <tang050405@gmail.com>
2023-08-01 15:51:16 +0900

    카카오맵 수정

--- src/main/resources/static/js/kty_map.js
+++ src/main/resources/static/js/kty_map.js
@@ -1,14 +1,14 @@
- var address = document.querySelector('.hoteladdress').innerText;
- 
-   var acc_hotel_name = document.querySelector('.hotelname').innerText;
+var address = document.querySelector('.hoteladdress').innerText;
 
-   
-   
+var acc_hotel_name = document.querySelector('.hotelname').innerText;
+
+
+
 var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
-    mapOption = {
-        center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
-        level: 3 // 지도의 확대 레벨
-    };  
+	mapOption = {
+		center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+		level: 3 // 지도의 확대 레벨
+	};
 // 지도를 생성합니다    
 var map = new kakao.maps.Map(mapContainer, mapOption);
 
@@ -18,24 +18,24 @@ var geocoder = new kakao.maps.services.Geocoder();
 // 주소로 좌표를 검색합니다
 geocoder.addressSearch(address, function(result, status) {
 
-    // 정상적으로 검색이 완료됐으면 
-     if (status === kakao.maps.services.Status.OK) {
+	// 정상적으로 검색이 완료됐으면 
+	if (status === kakao.maps.services.Status.OK) {
 
-        var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+		var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
 
-        // 결과값으로 받은 위치를 마커로 표시합니다
-        var marker = new kakao.maps.Marker({
-            map: map,
-            position: coords
-        });
-        
-   // 인포윈도우로 장소에 대한 설명을 표시합니다
-        var infowindow = new kakao.maps.InfoWindow({
-          content: '<div style="height: 30px; width: 200px; text-align: center; padding: 6px 0; display: flex; align-items: center; justify-content: center;">' + '<a href="https://map.kakao.com/?q=' + address + '" target="_blank">' + acc_hotel_name + '(길찾기)' + '</a>' + '</div>'
-        });
-        infowindow.open(map, marker);
+		// 결과값으로 받은 위치를 마s커로 표시합니다
+		var marker = new kakao.maps.Marker({
+			map: map,
+			position: coords
+		});
 
-        // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
-        map.setCenter(coords);
-    } 
+		// 인포윈도우로 장소에 대한 설명을 표시합니다
+		var infowindow = new kakao.maps.InfoWindow({
+			content: '<div style="height: 50px; width: 200px; text-align: center; padding: 6px 0; display: flex; align-items: center; justify-content: center; background-color: transparent;">' + '<a href="https://map.kakao.com/?q=' + address + '" target="_blank">' + acc_hotel_name + '(길찾기)' + '</a>' + '</div>'
+		});
+		infowindow.open(map, marker);
+
+		// 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
+		map.setCenter(coords);
+	}
 });     
