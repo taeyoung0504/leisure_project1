3421b36cb5bd282dd50a8d1fd2afe55df57e6279
Kim Do Gyeom <sexypig91@gmail.com>
2023-08-08 18:38:38 +0900

    bookStatus가 null이거나 공백이면 삭제

--- src/main/java/com/project/leisure/dogyeom/base/BookStatusUpdateService.java
+++ src/main/java/com/project/leisure/dogyeom/base/BookStatusUpdateService.java
@@ -50,7 +50,7 @@ public class BookStatusUpdateService {
 //    public void deleteNullBookStatus() {
 //        bookRepository.deleteByBookStatusIsNull();
 //    }
-    @Scheduled(fixedDelay = 120000) // 1분(1 * 60 * 1000)마다 실행
+    @Scheduled(fixedDelay = 90000) // 1분(1 * 60 * 1000)마다 실행
     @Transactional
     public void deleteNullBookStatus() {
     	bookRepository.deleteByBookStatusIsNullAndBookStatusIsEmpty();
